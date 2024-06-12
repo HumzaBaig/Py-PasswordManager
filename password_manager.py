f9d28e7c5e604ea88be2b34dc6600b5f64c7a8e1
HumzaBaig <humza.baig2009@gmail.com>
2024-06-12 13:08:48 -0500

    Create basic password manager functionality

---
+++ password_manager.py
@@ -0,0 +1,25 @@
+master_pwd = input("What is the master password? ")
+
+def view():
+    with open('passwords.txt', 'r') as f:
+        for line in f.readlines():
+            print(line.rstrip())
+
+def add():
+    name = input("Account Name: ")
+    password = input("Password: ")
+
+    with open('passwords.txt', 'a') as f:
+        f.write(name + "|" + password + "\n")
+
+while True:
+    mode = input("Would you like to add a new password, view existing ones, or quit? (view/add/q) ").lower()
+    if mode == "q":
+        break
+
+    if mode == "view":
+        view()
+    elif mode == "add":
+        add()
+    else:
+        break
